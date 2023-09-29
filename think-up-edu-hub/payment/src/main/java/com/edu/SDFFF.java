e7ace445aca079e1f661db7d4d0d6cf56bc22374
tjddn <tjddn3927@naver.com>
2023-09-29 20:45:18 +0900

    core 모듈의 다른 모듈 의존성 주

---
+++ think-up-edu-hub/payment/src/main/java/com/edu/SDFFF.java
@@ -0,0 +1,10 @@
+package com.edu;
+
+public class SDFFF {
+
+    private int dd;
+
+    public SDFFF(int dd) {
+        this.dd = dd;
+    }
+}
---
+++ think-up-edu-hub/user/src/main/java/com/edu/ASD.java
@@ -0,0 +1,10 @@
+package com.edu;
+
+public class ASD {
+
+    private int a;
+
+    public ASD(int a) {
+        this.a = a;
+    }
+}
