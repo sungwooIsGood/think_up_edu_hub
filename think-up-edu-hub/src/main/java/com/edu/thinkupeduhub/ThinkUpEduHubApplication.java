d94c0a2e36e2d5a0dfac5e20800ef3296fdee367
tjddn <tjddn3927@naver.com>
2023-09-29 12:33:50 +0900

    멀티모듈 구성

---
+++ think-up-edu-hub/src/main/java/com/edu/thinkupeduhub/ThinkUpEduHubApplication.java
@@ -0,0 +1,13 @@
+package com.edu.thinkupeduhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkUpEduHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkUpEduHubApplication.class, args);
+	}
+
+}
---
+++ think-up-edu-hub/src/test/java/com/edu/thinkupeduhub/ThinkUpEduHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.thinkupeduhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkUpEduHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
