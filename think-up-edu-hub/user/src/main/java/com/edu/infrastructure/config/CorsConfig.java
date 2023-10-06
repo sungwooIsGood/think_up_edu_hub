1ab243d3cc5c68573d09f879f4182933574fdd75
tjddn <tjddn3927@naver.com>
2023-10-06 17:09:21 +0900

    spring security config 관련 설정

---
+++ think-up-edu-hub/user/src/main/java/com/edu/infrastructure/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.edu.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    /**
+     * cors 조건을 security filter에 넣기 위해 생성
+     */
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 응답 시 json을 return 시 자바스크립트로 처리 가능하게
+        config.addAllowedOrigin("*"); // 모든 ip에서 가능하게
+        config.addAllowedHeader("*"); // 모든 header에서 가능하게
+        config.addAllowedMethod("*"); // 모든 HTTP 메서드 가능하게
+        source.registerCorsConfiguration("/",config);
+        return new CorsFilter(source);
+    }
+
+}
---
+++ think-up-edu-hub/user/src/main/java/com/edu/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.edu.infrastructure.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity // security 활성화
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsFilter CorsFilter; // corsConfig에서 빈으로 설정한 것
+
+    // PasswordEncoder는 BCryptPasswordEncoder를 사용
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // state less 방식, session 사용 x
+                .and()
+                .addFilter(CorsFilter)
+                .formLogin().disable() // 스프링 시큐리티에서 제공하는 로그인 폼을 사용하지 않을 것이므로 disable
+                // http 로그인 방식 사용x, headers에서 Authorization ID: ~~ / pwd: ~~ 이런식으로 암호화 안된 상태로 날라감. 보안 취약
+                // 그래서 jwt로 Bearer 방식 사용할 것, Bearer은 토큰 형식으로 날아가게 하는 것
+                .httpBasic().disable()
+                .authorizeRequests(); // Http 요청에 대한 접근권한 설정 메서드 특정 url 설정할 수 있으나 나는 추가 설정 x
+//                .anyRequest().authenticated(); // 그 외 인증 없이 접근X
+    }
+}
