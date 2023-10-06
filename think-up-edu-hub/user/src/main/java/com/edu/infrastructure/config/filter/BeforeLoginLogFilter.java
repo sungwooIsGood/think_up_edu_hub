9e93d0c43d33a9bd4f08e33fc595b58c2ef72da0
tjddn <tjddn3927@naver.com>
2023-10-06 18:21:51 +0900

    spring security와 jwt aop를 활용해보기 위해 설정 다시 수정

--- think-up-edu-hub/user/src/main/java/com/edu/infrastructure/config/filter/BeforeLoginLogFilter.java
+++ think-up-edu-hub/user/src/main/java/com/edu/infrastructure/config/filter/BeforeLoginLogFilter.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 public class BeforeLoginLogFilter implements Filter {
 
     @Override
-    @Deprecated
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 //        log.info("{} 호출한 IP",request.getRemoteAddr());
         chain.doFilter(request,response); // 커스텀 필터가 끝나고 다음 필터가 진행되게 하려면 꼭 해주어야한다.
